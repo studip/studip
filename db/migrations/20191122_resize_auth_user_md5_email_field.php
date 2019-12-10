15c109ff38d65fef32224c13a92e7472b6efa7fc
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2019-12-10 11:57:37 +0000

    changes in the wake of code review, re #9929
    git-svn-id: svn://develop.studip.de/studip/trunk@55376 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- db/migrations/20191122_resize_auth_user_md5_email_field.php
+++ db/migrations/20191122_resize_auth_user_md5_email_field.php
@@ -1,30 +1,25 @@
 <?php
-
-
 class ResizeAuthUserMd5EmailField extends Migration
 {
+    public function description()
+    {
+        return 'Resizes the Email field of the auth_user_md5 table to mail addresses with up to 256 characters, as specified in RFC3696.';
+    }
+
     public function up()
     {
-        $db = DBManager::get();
-        $db->exec(
+        DBManager::get()->exec(
             "ALTER TABLE `auth_user_md5`
-            CHANGE COLUMN `Email` `Email` VARCHAR(256) NULL DEFAULT NULL"
+             CHANGE COLUMN `Email` `Email` VARCHAR(256) NULL DEFAULT NULL"
         );
     }
 
 
     public function down()
     {
-        $db = DBManager::get();
-        $db->exec(
+        DBManager::get()->exec(
             "ALTER TABLE `auth_user_md5`
-            CHANGE COLUMN `Email` `Email` VARCHAR(256) NULL DEFAULT NULL"
+             CHANGE COLUMN `Email` `Email` VARCHAR(256) NULL DEFAULT NULL"
         );
-    }
-
-
-    public function description()
-    {
-        return 'Resizes the Email field of the auth_user_md5 table to mail addresses with up to 256 characters, as specified in RFC3696.';
     }
 }
