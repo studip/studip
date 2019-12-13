4726a90fec8db55a285de4a19fec451ebb15f41a
krassmus <krassmus@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2019-12-13 13:13:13 +0000

    re #8353 : fix arrow in widget
    git-svn-id: svn://develop.studip.de/studip/trunk@55484 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- public/assets/javascripts/studip-base.js
+++ public/assets/javascripts/studip-base.js
@@ -48,7 +48,7 @@
 /******/
 /******/ 	// script path function
 /******/ 	function jsonpScriptSrc(chunkId) {
-/******/ 		return __webpack_require__.p + "javascripts/" + ({"vendors~avatarcropper":"vendors~avatarcropper","vendors~code-highlight":"vendors~code-highlight","code-highlight":"code-highlight","vendors~tablesorter":"vendors~tablesorter","tablesorter":"tablesorter","vendors~widgetsystem":"vendors~widgetsystem","widgetsystem":"widgetsystem"}[chunkId]||chunkId) + "-" + {"vendors~avatarcropper":"c3f77350335a03ba616e","vendors~code-highlight":"3ba84552318339938734","code-highlight":"a0b618ee3d44ce7d4cbe","vendors~tablesorter":"130ec2dfc24115df9c7b","tablesorter":"e71c02e6efe545889fca","vendors~widgetsystem":"43e519d3201f49e16b11","widgetsystem":"d1844172668c54de6273"}[chunkId] + ".chunk.js"
+/******/ 		return __webpack_require__.p + "javascripts/" + ({"vendors~avatarcropper":"vendors~avatarcropper","vendors~code-highlight":"vendors~code-highlight","code-highlight":"code-highlight","vendors~tablesorter":"vendors~tablesorter","tablesorter":"tablesorter","vendors~widgetsystem":"vendors~widgetsystem","widgetsystem":"widgetsystem"}[chunkId]||chunkId) + "-" + {"vendors~avatarcropper":"c3f77350335a03ba616e","vendors~code-highlight":"e8ca6e6903590495e59d","code-highlight":"a0b618ee3d44ce7d4cbe","vendors~tablesorter":"130ec2dfc24115df9c7b","tablesorter":"e71c02e6efe545889fca","vendors~widgetsystem":"0f229a937ee12d09cb57","widgetsystem":"ae54335c4805f0d4f5fd"}[chunkId] + ".chunk.js"
 /******/ 	}
 /******/
 /******/ 	// The require function
