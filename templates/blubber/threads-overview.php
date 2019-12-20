0677317a85d6406bf4009c7ed20376ab63045a1e
krassmus <krassmus@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2019-12-20 09:37:03 +0000

    re #8353 : add singular if only one thread exists
    git-svn-id: svn://develop.studip.de/studip/trunk@55584 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- templates/blubber/threads-overview.php
+++ templates/blubber/threads-overview.php
@@ -8,7 +8,7 @@
                 </a>
             <? endif ?>
         </div>
-        <?= _("Konversationen") ?>
+        <?= count($json) > 1 ? _("Konversationen") : _("Konversation") ?>
     </div>
     <div class="sidebar-widget-content">
         <blubber-thread-widget :threads="threads" :active_thread="active_thread" :more_down="threads_more_down"></blubber-thread-widget>
