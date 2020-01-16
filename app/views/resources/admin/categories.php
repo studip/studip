f73f596f01120a70e46edaa0d019e16bc3199860
davka <davka@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2020-01-16 14:22:51 +0000

    refine categories-view, re #7509
    git-svn-id: svn://develop.studip.de/studip/trunk@55937 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/views/resources/admin/categories.php
+++ app/views/resources/admin/categories.php
@@ -46,28 +46,25 @@
                 <? endif ?>
             </td>
             <td class="actions">
-                <?
-                $actions = ActionMenu::get();
-                if ($category->hasResources()) {
-                    $actions->addLink(
+                <?php
+                $actions = ActionMenu::get()
+                    ->conditionAll($category->hasResources())
+                    ->addLink(
                         $controller->link_for(
                             'resources/category/show_resources/' . $category->id
                         ),
                         _('Alle Ressourcen anzeigen'),
                         Icon::create('log'),
-                        ['data-dialog' => 'size=auto']
-                    );
-                }
-                $actions->addLink(
-                    $controller->link_for(
-                        'resources/category/edit/' . $category->id
-                    ),
-                    _('Bearbeiten'),
-                    Icon::create('edit'),
-                    ['data-dialog' => 'size=auto']
-                );
-                if ($category->system == '0') {
-                    $actions->addLink(
+                        ['data-dialog' => 'size=auto'])
+                    ->addLink(
+                        $controller->link_for(
+                            'resources/category/edit/' . $category->id
+                        ),
+                        _('Bearbeiten'),
+                        Icon::create('edit'),
+                        ['data-dialog' => 'size=auto'])
+                    ->condition($category->system == '0')
+                    ->addLink(
                         $controller->link_for(
                             'resources/category/delete/' . $category->id
                         ),
@@ -75,9 +72,8 @@
                         Icon::create('trash'),
                         ['data-dialog' => 'size=auto']
                     );
-                }
+                    echo  $actions->render();
                 ?>
-                <?= $actions->render() ?>
             </td>
         </tr>
         <? endforeach ?>
