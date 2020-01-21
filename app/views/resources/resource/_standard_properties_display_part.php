c56a472a21cfcdd58f3d0e724e5a77653e64fd2d
davka <davka@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2020-01-21 15:41:56 +0000

    refine room-overview, re #7509
    git-svn-id: svn://develop.studip.de/studip/trunk@56000 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/views/resources/resource/_standard_properties_display_part.php
+++ app/views/resources/resource/_standard_properties_display_part.php
@@ -7,84 +7,95 @@
 ?>
 <? if ($property_groups): ?>
     <? foreach ($property_groups as $name => $properties): ?>
-    <? $properties = array_filter($properties, function ($p) {return !empty($p->state);}); ?>
-    <? if (!count($properties)) continue; ?>
-        <table class="default">
-            <caption>
-                <? if ($name): ?>
-                    <?= htmlReady($name) ?>
-                <? else: ?>
-                    <?= _('Weitere Eigenschaften') ?>
-                <? endif ?>
-            </caption>
-            <thead>
-                <tr>
-                    <th>
-                        <?= _('Name') ?>
-                    </th>
-                    <th>
-                        <?= _('Wert') ?>
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                <? foreach ($properties as $property): ?>
+        <? $properties = array_filter($properties, function ($p) {
+            return !empty($p->state);
+        }); ?>
+        <? if (!count($properties)) continue; ?>
+        <section class="contentbox">
+            <header>
+                <h1>
+                    <? if ($name): ?>
+                        <?= htmlReady($name) ?>
+                    <? else: ?>
+                        <?= _('Weitere Eigenschaften') ?>
+                    <? endif ?>
+                </h1>
+            </header>
+
+            <table class="default">
+                <colgroup>
+                    <col style="width: 70%">
+                    <col>
+                </colgroup>
+                <thead>
                     <tr>
-                        <td>
-                            <?= htmlReady(
-                                $property->display_name
-                                ? $property->display_name
-                                : $property->name
-                            ) ?>
-                        </td>
-                        <td>
-                            <? if ($property->definition->type == 'bool'): ?>
-                                <? if ($property->state): ?>
-                                    <?= _('ja')?>
-                                <? else: ?>
-                                    <?= _('nein')?>
-                                <? endif ?>
-                            <? elseif ($property->definition->type == 'user'): ?>
-                                <?
-                                $user = User::findByUsername($property->state);
-                                if (!$user) {
-                                    //Find by ID:
-                                    $user = User::find($property->state);
-                                }
-                                ?>
-                                <? if ($user instanceof User): ?>
-                                    <a href="<?= $controller->link_for(
-                                             'profile',
-                                             ['username' => $user->username]
-                                             )?>" target="_blank">
-                                        <?= htmlReady($user->getFullName()) ?>
-                                    </a>
-                                    <a href="<?= $controller->link_for(
-                                             'messages/write',
-                                             ['rec_uname' => $user->username]
-                                             )?>" data-dialog>
-                                        <?= Icon::create('mail')->asImg(
+                        <th>
+                            <?= _('Name') ?>
+                        </th>
+                        <th>
+                            <?= _('Wert') ?>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <? foreach ($properties as $property): ?>
+                        <tr>
+                            <td>
+                                <?= htmlReady(
+                                    $property->display_name
+                                        ? $property->display_name
+                                        : $property->name
+                                ) ?>
+                            </td>
+                            <td>
+                                <? if ($property->definition->type == 'bool'): ?>
+                                    <? if ($property->state): ?>
+                                        <?= _('ja') ?>
+                                    <? else: ?>
+                                        <?= _('nein') ?>
+                                    <? endif ?>
+                                <? elseif ($property->definition->type == 'user'): ?>
+                                    <?
+                                    $user = User::findByUsername($property->state);
+                                    if (!$user) {
+                                        //Find by ID:
+                                        $user = User::find($property->state);
+                                    }
+                                    ?>
+                                    <? if ($user instanceof User): ?>
+                                        <a href="<?= $controller->link_for(
+                                            'profile',
+                                            ['username' => $user->username]
+                                        ) ?>" target="_blank">
+                                            <?= htmlReady($user->getFullName()) ?>
+                                        </a>
+                                        <a href="<?= $controller->link_for(
+                                            'messages/write',
+                                            ['rec_uname' => $user->username]
+                                        ) ?>" data-dialog>
+                                            <?= Icon::create('mail')->asImg(
+                                                ['class' => 'text-bottom']
+                                            ) ?>
+                                        </a>
+                                    <? else: ?>
+                                        <?= htmlReady($property->state) ?>
+                                    <? endif ?>
+                                <? elseif ($property->definition->type == 'url'): ?>
+                                    <a href="<?= htmlReady($property->state) ?>"
+                                       target="_blank">
+                                        <?= htmlReady($property->state) ?>
+                                        <?= Icon::create('link-extern')->asImg(
                                             ['class' => 'text-bottom']
                                         ) ?>
                                     </a>
                                 <? else: ?>
                                     <?= htmlReady($property->state) ?>
                                 <? endif ?>
-                            <? elseif ($property->definition->type == 'url'): ?>
-                                <a href="<?= htmlReady($property->state) ?>"
-                                   target="_blank">
-                                    <?= htmlReady($property->state) ?>
-                                    <?= Icon::create('link-extern')->asImg(
-                                        ['class' => 'text-bottom']
-                                    ) ?>
-                                </a>
-                            <? else: ?>
-                                <?= htmlReady($property->state) ?>
-                            <? endif ?>
-                        </td>
-                    </tr>
-                <? endforeach ?>
-            </tbody>
-        </table>
+                            </td>
+                        </tr>
+                    <? endforeach ?>
+                </tbody>
+            </table>
+        </section>
     <? endforeach ?>
 <? endif ?>
--- app/views/resources/room/index.php
+++ app/views/resources/room/index.php
@@ -1,12 +1,12 @@
 <nav class="resource-hierarchy">
     <? if ($room->building->location): ?>
-        <a href="<?= $room->building->location->getLink('show') ?>" <?= (Request::isDialog())?'data-dialog':''; ?> >
-            <?= htmlReady($room->building->location->name)?>
+        <a href="<?= $room->building->location->getLink('show') ?>" <?= (Request::isDialog()) ? 'data-dialog' : ''; ?>>
+            <?= htmlReady($room->building->location->name) ?>
         </a>
         &gt;
     <? endif ?>
     <? if ($room->building): ?>
-        <a href="<?= $room->building->getLink('show') ?>" <?= (Request::isDialog())?'data-dialog':''; ?> >
+        <a href="<?= $room->building->getLink('show') ?>" <?= (Request::isDialog()) ? 'data-dialog' : ''; ?>>
             <?= htmlReady($room->building->name) ?>
         </a>
         &gt;
@@ -40,35 +40,40 @@
                 ResourceManager::getMapUrlForResourcePosition(
                     $room->building->getPropertyObject('geo_coordinates')
                 ),
-                ["target"=> "_blank"] ) ?>
+                ["target" => "_blank"]) ?>
         <? endif ?>
         <? if ($room->requestable) : ?>
             <?= \Studip\LinkButton::create(
                 _('Raum anfragen'),
                 $room->getLink('request'),
-                ['data-dialog' => 'size=auto'] ) ?>
+                ['data-dialog' => 'size=auto']) ?>
         <? endif ?>
     </div>
 <? endif ?>
 
-<? if ($room->description): ?>
-    <h3><?= _('Beschreibung und Hinweise') ?></h3>
-    <p class="resource-description-text"><?= htmlReady($room->description) ?></p>
-<? endif ?>
-<section>
-    <ul>
-        <? if ($room->room_type): ?>
-            <li><?= htmlReady($room->room_type) ?></li>
+
+<section class="contentbox">
+    <header>
+        <h1><?= _('Beschreibung und Hinweise') ?></h1>
+    </header>
+    <section>
+        <? if ($room->description): ?>
+            <p><?= htmlReady($room->description) ?></p>
         <? endif ?>
-        <li>
-            <?= sprintf(
-                ngettext('%d Sitzplatz', '%d Sitzplätze', intval($room->seats)), intval($room->seats)
-            ) ?>
-        </li>
-    </ul>
+        <ul>
+            <? if ($room->room_type): ?>
+                <li><?= htmlReady($room->room_type) ?></li>
+            <? endif ?>
+            <li>
+                <?= sprintf(
+                    ngettext('%d Sitzplatz', '%d Sitzplätze', intval($room->seats)), intval($room->seats)
+                ) ?>
+            </li>
+        </ul>
+    </section>
 </section>
+
 <? if ($grouped_properties): ?>
-    <h3><?= _('Raumeigenschaften') ?></h3>
     <?= $this->render_partial(
         'resources/resource/_standard_properties_display_part.php',
         [
@@ -78,17 +83,21 @@
 <? endif ?>
 
 <? $resource_folder = $room->getFolder(); ?>
-<? if($resource_folder && $resource_folder->getFiles()): ?>
-<h2><?= _('Dateien') ?></h2>
-    <table class="default sortable-table" data-sortlist="[[2, 0]]">
-        <?= $this->render_partial('files/_files_thead') ?>
-        <? foreach($resource_folder->getFiles() as $file_ref): ?>
-            <?= $this->render_partial('files/_fileref_tr',
-                [
-                    'file_ref' => $file_ref,
-                    'current_folder' => $resource_folder,
-                    'last_visitdate' => time()
-                ]) ?>
-        <? endforeach ?>
-    </table>
+<? if ($resource_folder && $resource_folder->getFiles()): ?>
+   <section class="contentbox">
+       <header>
+           <h1><?= _('Dateien') ?></h1>
+       </header>
+       <table class="default sortable-table" data-sortlist="[[2, 0]]">
+           <?= $this->render_partial('files/_files_thead') ?>
+           <? foreach ($resource_folder->getFiles() as $file_ref): ?>
+               <?= $this->render_partial('files/_fileref_tr',
+                   [
+                       'file_ref'       => $file_ref,
+                       'current_folder' => $resource_folder,
+                       'last_visitdate' => time()
+                   ]) ?>
+           <? endforeach ?>
+       </table>
+   </section>
 <? endif ?>
