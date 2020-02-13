23e41cf8b03b63024d9165347530b5034aae8151
mlunzena <mlunzena@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2020-02-13 07:14:48 +0000

    build: re-build all the static JS and CSS files, refs #10400, #10401
    git-svn-id: svn://develop.studip.de/studip/trunk@56334 c5b85792-5220-0410-ad23-ed5b6b8ad999

---
+++ public/assets/javascripts/tablesorter-889fdfaf180aea5ee6e4.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{"c/R+":function(a,r,t){"use strict";t.r(r);t("nQco"),t("3zzX"),t("WPsp");jQuery.tablesorter.addParser({id:"htmldata",is:function(a,r,t){var e=r.config.parserMetadataName||"sortValue";return void 0!==$(t).data(e)},format:function(a,r,t){var e=r.config.parserMetadataName||"sortValue";return $(t).data(e)},type:"numeric"})}}]);
+//# sourceMappingURL=tablesorter-889fdfaf180aea5ee6e4.chunk.js.map
