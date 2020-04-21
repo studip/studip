0bdeadfd05529488ca9f546b3cfa2169f16ed371
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2020-04-21 05:40:49 +0000

    fix regression from [56910], fixes #10498
    git-svn-id: svn://develop.studip.de/studip/trunk@56921 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/controllers/my_courses.php
+++ app/controllers/my_courses.php
@@ -409,6 +409,10 @@ class MyCoursesController extends AuthenticatedController
         }
 
         $deputies_enabled = Config::get()->DEPUTIES_ENABLE;
+        $GLOBALS['user']->cfg->store(
+            'MY_COURSES_GROUPING',
+            Request::get('select_group_field', $GLOBALS['user']->cfg->MY_COURSES_GROUPING)
+        );
         $gruppe = Request::getArray('gruppe');
         if (!empty($gruppe)) {
             $query          = "UPDATE seminar_user SET gruppe = ? WHERE Seminar_id = ? AND user_id = ?";
