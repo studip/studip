523cc22630da1956504c8cdbef16a3470bf7e05a
mlunzena <mlunzena@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2020-04-28 05:09:52 +0000

    fix brittle jsonapi test case, fixes #10514
    git-svn-id: svn://develop.studip.de/studip/trunk@56999 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- tests/jsonapi/UserEventsIndexTest.php
+++ tests/jsonapi/UserEventsIndexTest.php
@@ -22,7 +22,39 @@ class UserEventsIndexTest extends \Codeception\Test\Unit
     public function testIndexUserEvents()
     {
         $credentials = $this->tester->getCredentialsForTestAutor();
+        $response = $this->getEvents($credentials);
+        $this->assertSuccessfulResponse($response);
 
+        $response = $this->getEvents($credentials);
+        $this->assertSuccessfulResponse($response);
+        $numEvents = count($response->document()->primaryResources());
+
+        $this->createEvent($credentials);
+
+        $response = $this->getEvents($credentials);
+        $this->assertSuccessfulResponse($response);
+        $this->tester->assertCount($numEvents + 1, $response->document()->primaryResources());
+    }
+
+    private function getEvents($credentials)
+    {
+        $app = $this->tester->createApp($credentials, 'get', '/users/{id}/events', UserEventsIndex::class);
+
+        $requestBuilder = $this->tester->createRequestBuilder($credentials);
+        $requestBuilder->setUri('/users/' . $credentials['id'] . '/events')->fetch();
+
+        return $this->tester->sendMockRequest($app, $requestBuilder->getRequest());
+    }
+
+    private function assertSuccessfulResponse($response)
+    {
+        $this->tester->assertTrue($response->isSuccessfulDocument([200]));
+        $document = $response->document();
+        $this->tester->assertTrue($document->isResourceCollectionDocument());
+    }
+
+    private function createEvent($credentials)
+    {
         $calendar = new \SingleCalendar($credentials['id']);
         $event = $calendar->getNewEvent();
 
@@ -32,18 +64,5 @@ class UserEventsIndexTest extends \Codeception\Test\Unit
         $calendar->storeEvent($event, [$credentials['id']]);
 
         $GLOBALS['user'] = $oldUser;
-
-        $app = $this->tester->createApp($credentials, 'get', '/users/{id}/events', UserEventsIndex::class);
-
-        $requestBuilder = $this->tester->createRequestBuilder($credentials);
-        $requestBuilder->setUri('/users/'.$credentials['id'].'/events')->fetch();
-
-        $response = $this->tester->sendMockRequest($app, $requestBuilder->getRequest());
-
-        $this->tester->assertTrue($response->isSuccessfulDocument([200]));
-        $document = $response->document();
-        $this->tester->assertTrue($document->isResourceCollectionDocument());
-        $resources = $document->primaryResources();
-        $this->tester->assertCount(1, $resources);
     }
 }
