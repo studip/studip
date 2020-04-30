58792bb3a9b20fe303eb8d0ed64a9080929016ef
strohm <strohm@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2020-04-30 12:28:11 +0000

    ResourceTreeWidget: only display the top nodes of the tree, re #10468
    git-svn-id: svn://develop.studip.de/studip/trunk@57050 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/classes/sidebar/ResourceTreeWidget.class.php
+++ lib/classes/sidebar/ResourceTreeWidget.class.php
@@ -113,7 +113,7 @@ class ResourceTreeWidget extends SidebarWidget
             }
         }
         $variables['resource_path'] = $resource_path;
-        $variables['max_open_depth'] = 1;
+        $variables['max_open_depth'] = 0;
         $variables['layout_css_classes'] = $this->layout_css_classes;
 
         $template = $GLOBALS['template_factory']->open(
