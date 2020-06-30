5edf8428a3939c5b7f7d434aa7f4e58def168bc2
krassmus <krassmus@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2020-06-30 15:06:27 +0000

    fixes #10630
    git-svn-id: svn://develop.studip.de/studip/trunk@57535 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/controllers/admin/domain.php
+++ app/controllers/admin/domain.php
@@ -69,7 +69,7 @@ class Admin_DomainController extends AuthenticatedController
     public function validate_args(&$args, $types = NULL)
     {
         foreach ($args as $arg) {
-            if (!preg_match('/' . UserDomain::REGEXP . '/', $arg)) {
+            if ($arg && !preg_match('/' . UserDomain::REGEXP . '/', $arg)) {
                 throw new Trails_Exception(400);
             }
         }
