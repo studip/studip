93af2d20eda3fd17fae40cb90866e16b09611e3b
strohm <strohm@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2020-07-13 08:24:59 +0000

    added config entry for ADMIN_COURSES_SIDEBAR_ACTIVE_ELEMENTS, re #7797
    git-svn-id: svn://develop.studip.de/studip/trunk@57632 c5b85792-5220-0410-ad23-ed5b6b8ad999

---
+++ db/migrations/20200713_add_admin_courses_sidebar_active_elements.php
@@ -0,0 +1,36 @@
+<?php
+
+
+class AddAdminCoursesSidebarActiveElements extends Migration
+{
+    public function up()
+    {
+        $c = DBManager::get();
+
+        $c->exec(
+            "INSERT INTO `config`
+            (`field`, `value`, `type`, `range`, `description`)
+            VALUES
+            (
+                'ADMIN_COURSES_SIDEBAR_ACTIVE_ELEMENTS',
+                '',
+                'string',
+                'user',
+                'Diese Einstellung legt fest, welche Elemente in der Seitenleiste der Veranstaltungsübersicht für Admins sichtbar ist. Diese Einstellung kann individuell vorgenommen werden.'
+            )"
+        );
+    }
+
+
+    Public function down()
+    {
+        $c = DBManager::get();
+
+        $c->exec(
+            "DELETE FROM `config` WHERE `field` = 'ADMIN_COURSES_SIDEBAR_ACTIVE_ELEMENTS'"
+        );
+        $c->exec(
+            "DELETE FROM `config_values` where `field` = 'ADMIN_COURSES_SIDEBAR_ACTIVE_ELEMENTS'"
+        );
+    }
+}
