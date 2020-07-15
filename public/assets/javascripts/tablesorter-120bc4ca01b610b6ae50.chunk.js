61ab458be111bb95bcc8d5e3c36681b1a9891690
anoack <anoack@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2020-07-15 16:43:30 +0000

    recreate assets remove obsolete re #10546
    git-svn-id: svn://develop.studip.de/studip/trunk@57660 c5b85792-5220-0410-ad23-ed5b6b8ad999

---
+++ public/assets/javascripts/tablesorter-120bc4ca01b610b6ae50.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{192:function(a,r,t){"use strict";t.r(r);t(225),t(226),t(227);jQuery.tablesorter.addParser({id:"htmldata",is:function(a,r,t,e){var n=r.config.parserMetadataName||"sortValue";return void 0!==$(t).data(n)},format:function(a,r,t){var e=r.config.parserMetadataName||"sortValue";return $(t).data(e)},type:"numeric"})}}]);
+//# sourceMappingURL=tablesorter-120bc4ca01b610b6ae50.chunk.js.map
