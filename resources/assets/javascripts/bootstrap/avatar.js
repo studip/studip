d3c6f791f23fcd6c1b910be519e941ee41a7b72d
Farbod <Farbod@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2020-08-12 15:17:22 +0000

    bugfix: avatar drag&drop null error, refs #9284
    git-svn-id: svn://develop.studip.de/studip/trunk@57850 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- resources/assets/javascripts/bootstrap/avatar.js
+++ resources/assets/javascripts/bootstrap/avatar.js
@@ -5,40 +5,42 @@ STUDIP.domReady(() => {
     // Get file data on drop
     var dropZone = document.getElementById('avatar-overlay');
 
-    dropZone.addEventListener('dragover', function(e) {
-        e.stopPropagation();
-        e.preventDefault();
-        e.target.parentNode.classList.add("dragging");
-    });
-
-    dropZone.addEventListener('dragleave', function(e) {
-        e.stopPropagation();
-        e.preventDefault();
-        e.target.parentNode.classList.remove("dragging");
-    });
-
-    dropZone.addEventListener('drop', function(e) {
-        e.stopPropagation();
-        e.preventDefault();
-        e.target.parentNode.classList.remove("dragging");
-        var files = e.dataTransfer.files;
-        var div = e.target.parentNode;
-        var avatar_dialog = div.getElementsByTagName('a')[0];
-
-        if (!div.getAttribute('accept') || !div.getAttribute('accept').includes(files[0].type)) {
-            alert(div.getAttribute('data-message-unaccepted'));
-            return false;
-        }
-
-        if (!div.getAttribute('data-max-size') || files[0].size > div.getAttribute('data-max-size')) {
-            alert(div.getAttribute('data-message-too-large'));
-            return false;
-        }
-
-        avatar_dialog.click();
-        div.files = files;
-        STUDIP.dialogReady(() => {
-            STUDIP.Avatar.readFile(div);
+    if (dropZone) {
+        dropZone.addEventListener('dragover', function(e) {
+            e.stopPropagation();
+            e.preventDefault();
+            e.target.parentNode.classList.add("dragging");
         });
-    });
+
+        dropZone.addEventListener('dragleave', function(e) {
+            e.stopPropagation();
+            e.preventDefault();
+            e.target.parentNode.classList.remove("dragging");
+        });
+
+        dropZone.addEventListener('drop', function(e) {
+            e.stopPropagation();
+            e.preventDefault();
+            e.target.parentNode.classList.remove("dragging");
+            var files = e.dataTransfer.files;
+            var div = e.target.parentNode;
+            var avatar_dialog = div.getElementsByTagName('a')[0];
+
+            if (!div.getAttribute('accept') || !div.getAttribute('accept').includes(files[0].type)) {
+                alert(div.getAttribute('data-message-unaccepted'));
+                return false;
+            }
+
+            if (!div.getAttribute('data-max-size') || files[0].size > div.getAttribute('data-max-size')) {
+                alert(div.getAttribute('data-message-too-large'));
+                return false;
+            }
+
+            avatar_dialog.click();
+            div.files = files;
+            STUDIP.dialogReady(() => {
+                STUDIP.Avatar.readFile(div);
+            });
+        });
+    }
 });
