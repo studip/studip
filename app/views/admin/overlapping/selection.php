9cec5e5fab9a315adfc2c60a465279265bf66c41
Farbod <Farbod@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2020-08-27 11:43:52 +0000

    apply studip fieldset display format, fixes #10665
    git-svn-id: svn://develop.studip.de/studip/trunk@58051 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/views/admin/overlapping/selection.php
+++ app/views/admin/overlapping/selection.php
@@ -5,53 +5,55 @@
         </legend>
 
         <label for="base-version-select">
-            <?= _('Studiengangteil') ?>
+            <span><?= _('Studiengangteil') ?></span>
+            <select id="base-version-select" class="nested-select" name="base_version">
+            <? if ($base_version) : ?>
+                <option value="<?= $base_version->id ?>" selected><?= htmlReady($base_version->getDisplayName()) ?></option>
+            <? endif; ?>
+            </select>
         </label>
-        <select id="base-version-select" class="nested-select" name="base_version">
-        <? if ($base_version) : ?>
-            <option value="<?= $base_version->id ?>" selected><?= htmlReady($base_version->getDisplayName()) ?></option>
-        <? endif; ?>
-        </select>
+
 
         <label for="comp-versions-select">
-            <?= _('Vergleichs-Studiengangteile') ?>
+            <span><?= _('Vergleichs-Studiengangteile') ?></span>
+            <select id="comp-versions-select" class="nested-select" name="comp_versions[]" multiple>
+            <? if (count($comp_versions)) : ?>
+                <? foreach($comp_versions as $comp_version) : ?>
+                    <option value="<?= $comp_version->id ?>" selected><?= htmlReady($comp_version->getDisplayName()) ?></option>
+                <? endforeach; ?>
+            <? endif; ?>
+            </select>
         </label>
-        <select id="comp-versions-select" class="nested-select" name="comp_versions[]" multiple>
-        <? if (count($comp_versions)) : ?>
-            <? foreach($comp_versions as $comp_version) : ?>
-                <option value="<?= $comp_version->id ?>" selected><?= htmlReady($comp_version->getDisplayName()) ?></option>
-            <? endforeach; ?>
-        <? endif; ?>
-        </select>
-        
+
+
         <label for="fachsem-select">
-            <?= _('Fachsemester') ?>
+            <span><?= _('Fachsemester') ?></span>
+            <select id="fachsem-select" class="nested-select" name="fachsems[]" multiple>
+                <? foreach (range(1, 6) as $fsem) : ?>
+                    <option value="<?= $fsem ?>"<?= in_array($fsem, (array) $fachsems) ? ' selected' : '' ?>>
+                    <?= $fsem . ModuleManagementModel::getLocaleOrdinalNumberSuffix($fsem) . ' ' . _('Fachsemester') ?>
+                    </option>
+                <? endforeach; ?>
+            </select>
         </label>
-        <select id="fachsem-select" class="nested-select" name="fachsems[]" multiple>
-            <? foreach (range(1, 6) as $fsem) : ?>
-                <option value="<?= $fsem ?>"<?= in_array($fsem, (array) $fachsems) ? ' selected' : '' ?>>
-                <?= $fsem . ModuleManagementModel::getLocaleOrdinalNumberSuffix($fsem) . ' ' . _('Fachsemester') ?>
-                </option>
-            <? endforeach; ?>
-        </select>
 
         <label for="semtype-select">
-            <?= _('Veranstaltungstyp-Filter') ?>
+            <span><?= _('Veranstaltungstyp-Filter') ?></span>
+            <select id="semtype-select" class="nested-select" name="semtypes[]" multiple>
+                <? foreach ($GLOBALS['SEM_CLASS'] as $class_id => $class) : ?>
+                    <? if ($class['studygroup_mode']) : continue;
+                    endif; ?>
+                    <optgroup class="nested-item-header" label="<?= htmlReady($class['name']) ?>">
+                        <? foreach ($class->getSemTypes() as $id => $type) : ?>
+                            <option class="nested-item nested-item-level-2"
+                                    value="<?= $id ?>"<?= in_array($id, (array) $semtypes) ? ' selected' : '' ?>>
+                                <?= htmlReady($type['name']) ?>
+                            </option>
+                        <? endforeach; ?>
+                    </optgroup>
+                <? endforeach; ?>
+            </select>
         </label>
-        <select id="semtype-select" class="nested-select" name="semtypes[]" multiple>
-            <? foreach ($GLOBALS['SEM_CLASS'] as $class_id => $class) : ?>
-                <? if ($class['studygroup_mode']) : continue;
-                endif; ?>
-                <optgroup class="nested-item-header" label="<?= htmlReady($class['name']) ?>">
-                    <? foreach ($class->getSemTypes() as $id => $type) : ?>
-                        <option class="nested-item nested-item-level-2"
-                                value="<?= $id ?>"<?= in_array($id, (array) $semtypes) ? ' selected' : '' ?>>
-                            <?= htmlReady($type['name']) ?>
-                        </option>
-                    <? endforeach; ?>
-                </optgroup>
-            <? endforeach; ?>
-        </select>
 
         <label>
             <input type="checkbox"
