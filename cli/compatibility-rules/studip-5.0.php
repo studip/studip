64e2e5c5d2dd0c34a00466b5776a06b38d073d37
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2020-11-11 11:19:29 +0000

    add another batch of compatibility rules for stud.ip 5.0, re #10918
    git-svn-id: svn://develop.studip.de/studip/trunk@59152 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- cli/compatibility-rules/studip-5.0.php
+++ cli/compatibility-rules/studip-5.0.php
@@ -4,5 +4,22 @@ return [
     'ScheduleDidUpdate' => 'Parameters for Notification have changed. You will now receive a #{yellow:Schedule object} instead of user_id and values array.',
     'ScheduleDidCreate' => 'Parameters for Notification have changed. You will now receive a #{yellow:Schedule object} instead of user_id and values array.',
 
+    'SemesterData'      => '#{red:Removed} - use #{yellow:Semester model} instead',
+    'StatusgroupsModel' => '#{red:Removed} - use #{yellow:Statusgruppen model} instead',
+    'StudipNullCache'   => '#{red:Removed} - use #{yellow:StudipMemoryCache} instead',
+
     'get_config' => '#{red:Deprecated} - use #{yellow:Config::get()} instead',
+
+    // Vendor changes
+    'Leafo\\ScssPhp' => 'Library was replaced by #{yellow:scssphp/scssphp}',
+    'sfYamlParser'   => 'Library was replaced by #{yellow:symfony/yaml}',
+    'DocBlock::of'   => 'Library was replaced by #{yellow:gossi/docblock}',
+
+    'vendor/idna_convert' => 'Remove include/require. Will be autoloaded.',
+    'vendor/php-htmldiff' => 'Remove include/require. Will be autoloaded.',
+    'vendor/HTMLPurifier' => 'Remove include/require. Will be autoloaded.',
+    'vendor/phplot'       => 'Remove include/require. Will be autoloaded.',
+    'vendor/phpCAS'       => 'Remove include/require. Will be autoloaded.',
+    'vendor/phpxmlrpc'    => 'Remove include/require. Will be autoloaded.',
+
 ];
