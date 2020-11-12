49ee7e06f931f1069853d20f0e51f1d2580fb203
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2020-11-12 21:08:51 +0000

    align colors of planer and my courses but keep yellow as default, fixes #9101
    git-svn-id: svn://develop.studip.de/studip/trunk@59195 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/classes/calendar/CalendarScheduleModel.php
+++ lib/classes/calendar/CalendarScheduleModel.php
@@ -18,15 +18,15 @@ define('DEFAULT_COLOR_SEM', 2);
 define('DEFAULT_COLOR_NEW', 3);
 define('DEFAULT_COLOR_VIRTUAL', 1);
 define('CALENDAR_COLOR_MAPPING', [
-    0 => 9,
-    1 => 8,
-    2 => 5,
-    3 => 14,
-    4 => 7,
-    5 => 10,
-    6 => 11,
-    7 => 2,
-    8 => 2,
+    0 => 2,
+    1 => 3,
+    2 => 8,
+    3 => 5,
+    4 => 9,
+    5 => 7,
+    6 => 1,
+    7 => 4,
+    8 => 6,
 ]);
 
 /**
