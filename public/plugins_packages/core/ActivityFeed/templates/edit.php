ade372ba27311a83ff1f46a344cf83c887b3cd51
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2020-11-20 14:30:27 +0000

    resurrect legend elements, fixes #10955
    git-svn-id: svn://develop.studip.de/studip/trunk@59331 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- public/plugins_packages/core/ActivityFeed/templates/edit.php
+++ public/plugins_packages/core/ActivityFeed/templates/edit.php
@@ -1,21 +1,20 @@
 <div id="activityEdit">
-    <form id="configure_activity" action="<?= PluginEngine::getURL($plugin, [], 'save') ?>" method="post" class="default" data-dialog>
-        <fieldset>
-            <legend><?= _("Anzuzeigende Bereiche:") ?></legend>
+    <form id="configure_activity" action="<?= PluginEngine::getLink($plugin, [], 'save') ?>" method="post" class="default" data-dialog>
+        <h1><?= _("Anzuzeigende Bereiche:") ?></h1>
 
-            <? foreach ($modules as $context => $provider) : ?>
-            <fieldset>
-                <legend><?= htmlReady($context_translations[$context]) ?></legend>
-                <? foreach ($provider as $prv_id => $prv_name) : ?>
-                <label>
-                    <input type="checkbox" name="provider[<?= $context ?>][]" value="<?= htmlReady($prv_id) ?>"
-                        <?= empty($config) || (is_array($config[$context]) && in_array($prv_id, $config[$context])) ? 'checked' : ''?>>
-                    <?= htmlReady($prv_name) ?>
-                </label>
-                <? endforeach ?>
-            </fieldset>
+    <? foreach ($modules as $context => $provider): ?>
+        <fieldset>
+            <legend><?= htmlReady($context_translations[$context]) ?></legend>
+            <? foreach ($provider as $prv_id => $prv_name) : ?>
+            <label>
+                <input type="checkbox" name="provider[<?= $context ?>][]" value="<?= htmlReady($prv_id) ?>"
+                    <?= empty($config) || (is_array($config[$context]) && in_array($prv_id, $config[$context])) ? 'checked' : ''?>>
+                <?= htmlReady($prv_name) ?>
+            </label>
             <? endforeach ?>
         </fieldset>
+    <? endforeach; ?>
+
         <footer data-dialog-button>
             <?= Studip\Button::createAccept(_('Speichern')) ?>
             <?= Studip\Button::createCancel(_('Abbrechen'), URLHelper::getLink('dispatch.php/start')) ?>
