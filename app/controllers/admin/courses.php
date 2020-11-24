39a29466f462cbe76f252aa8ca2d07a1fc64d83b
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2020-11-24 13:43:13 +0000

    re #10844
    git-svn-id: svn://develop.studip.de/studip/trunk@59362 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/controllers/admin/courses.php
+++ app/controllers/admin/courses.php
@@ -1206,6 +1206,8 @@ class Admin_CoursesController extends AuthenticatedController
                 $seminars[$seminar_id]['dozenten'] = $dozenten;
 
                 if (in_array('contents', $params['view_filter'])) {
+                    $seminars[$seminar_id]['visitdate'] = $visits[$seminar_id]['sem']['visitdate'];
+                    $seminars[$seminar_id]['last_visitdate'] = $visits[$seminar_id]['sem']['last_visitdate'];
                     $seminars[$seminar_id]['sem_class'] = $sem_types[$seminar['status']]->getClass();
                     $seminars[$seminar_id]['modules'] = $modules->getLocalModules(
                         $seminar_id,
