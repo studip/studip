28880901789ad11c98b8151f525455b01bbdc0d7
hackl <hackl@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2020-11-25 12:31:16 +0000

    create system notifications on resource booking lifecycle, fixes #10966
    git-svn-id: svn://develop.studip.de/studip/trunk@59385 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/models/resources/ResourceBooking.class.php
+++ lib/models/resources/ResourceBooking.class.php
@@ -96,6 +96,14 @@ class ResourceBooking extends SimpleORMap implements PrivacyObject, Studip\Calen
 
         $config['registered_callbacks']['after_store'][] = 'updateIntervals';
 
+        // Some notifications that follow a booking lifecycle
+        $config['registered_callbacks']['before_create'][] = 'cbPostNotification';
+        $config['registered_callbacks']['after_create'][] = 'cbPostNotification';
+        $config['registered_callbacks']['before_store'][] = 'cbPostNotification';
+        $config['registered_callbacks']['after_store'][] = 'cbPostNotification';
+        $config['registered_callbacks']['before_delete'][] = 'cbPostNotification';
+        $config['registered_callbacks']['after_delete'][] = 'cbPostNotification';
+
         //In regard to TIC 6460:
         //As long as TIC 6460 is not implemented, we must add the validate
         //method as a callback before storing the object.
@@ -842,8 +850,8 @@ class ResourceBooking extends SimpleORMap implements PrivacyObject, Studip\Calen
         }
         return $deleted_c;
     }
-    
-    
+
+
     /**
      * Determines whether the resource booking ends on the same timestamp
      * like the lecture time of one of the defined semesters.
@@ -1511,8 +1519,8 @@ class ResourceBooking extends SimpleORMap implements PrivacyObject, Studip\Calen
         }
         return $strings;
     }
-    
-    
+
+
     /**
      * @inheritDoc
      */
@@ -1787,4 +1795,37 @@ class ResourceBooking extends SimpleORMap implements PrivacyObject, Studip\Calen
             }
         }
     }
+
+    public function cbPostNotification($type)
+    {
+        $data = [
+            'booking_id' => $this->id,
+            'start' => $this->begin,
+            'end' => $this->end,
+            'resource' => $this->resource_id
+        ];
+        switch ($type) {
+            case 'before_create':
+                $event = 'ResourceBookingWillCreate';
+                break;
+            case 'after_create':
+                $event = 'ResourceBookingDidCreate';
+                break;
+            case 'before_store':
+                $event = 'ResourceBookingWillUpdate';
+                break;
+            case 'after_store':
+                $event = 'ResourceBookingDidUpdate';
+                break;
+            case 'before_delete':
+                $event = 'ResourceBookingWillDelete';
+                break;
+            case 'after_delete':
+                $event = 'ResourceBookingDidDelete';
+                break;
+        }
+
+        NotificationCenter::postNotification($event, $data);
+    }
+
 }
