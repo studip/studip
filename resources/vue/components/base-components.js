bf3670fde489f8b8df7d4c286f742840d47a63a7
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2020-12-08 18:24:41 +0000

    comment non existing component for now, re #10990
    git-svn-id: svn://develop.studip.de/studip/trunk@59533 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- resources/vue/components/base-components.js
+++ resources/vue/components/base-components.js
@@ -2,7 +2,7 @@ import StudipAssetImg from './StudipAssetImg.vue';
 import StudipDateTime from './StudipDateTime.vue';
 import StudipFileSize from './StudipFileSize.vue';
 import StudipIcon from './StudipIcon.vue';
-import StudipLoadingIndicator from './StudipLoadingIndicator.vue';
+// import StudipLoadingIndicator from './StudipLoadingIndicator.vue';
 import StudipMessageBox from './StudipMessageBox.vue';
 
 const BaseComponents = {
