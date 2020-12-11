c726c17ece81187cc81b318cc88e92e17c989791
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2020-12-11 09:10:52 +0000

    fixes #10938
    git-svn-id: svn://develop.studip.de/studip/trunk@59580 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- templates/widgets-new/wrapper.php
+++
@@ -1,4 +0,0 @@
- <div class="grid-stack-item-content has-layout widget-<?= strtosnakecase(get_class($widget), '-') ?> <? if (!$widget->enabled) echo 'widget-disabled'; ?>">
-     <?= $content_for_layout ?>
-     <?= $actions ?>
- </div>
