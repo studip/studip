9e93ff0e53b62e65cc049c8d0a935f8f3bd38108
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2020-12-14 08:56:58 +0000

    fix older biest migration, re #9614
    git-svn-id: svn://develop.studip.de/studip/trunk@59614 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- db/migrations/20190917_fix_missing_consultation_events.php
+++ db/migrations/20190917_fix_missing_consultation_events.php
@@ -16,11 +16,70 @@ class FixMissingConsultationEvents extends Migration
                   WHERE teacher_event_id IS NULL";
         $ids = DBManager::get()->fetchFirst($query);
 
-        ConsultationSlot::findAndMapMany(
+        LegacyConsultationSlot::findAndMapMany(
             function ($slot) {
                 $slot->updateEvent();
             },
             $ids
         );
+    }
+}
+
+class LegacyConsultationSlot extends ConsultationSlot
+{
+    /**
+     * Updates the teacher event that belongs to the slot. This will either be
+     * set to be unoccupied, occupied by only one user or by a group of user.
+     */
+    public function updateEvent()
+    {
+        if (count($this->bookings) === 0 && !$this->block->calendar_events) {
+            return $this->removeEvent();
+        }
+
+        $teacher = User::find($this->block->teacher_id);
+        if (!$teacher) {
+            return;
+        }
+
+        $event = $this->event;
+        if (!$event) {
+            $event = $this->createEvent($teacher);
+
+            $this->teacher_event_id = $event->id;
+            $this->store();
+        }
+
+        setTempLanguage($teacher->id);
+
+        if (count($this->bookings) > 0) {
+            $event->category_intern = 1;
+
+            if (count($this->bookings) === 1) {
+                $booking = $this->bookings->first();
+
+                $event->summary = sprintf(
+                    _('Sprechstundentermin mit %s'),
+                    $booking->user->getFullName()
+                );
+                $event->description = $booking->reason;
+            } else {
+                $event->summary = sprintf(
+                    _('Sprechstundentermin mit %u Personen'),
+                    count($this->bookings)
+                );
+                $event->description = implode("\n\n----\n\n", $this->bookings->map(function ($booking) {
+                    return "- {$booking->user->getFullName()}:\n{$booking->reason}";
+                }));
+            }
+        } else {
+            $event->category_intern = 9;
+            $event->summary         = _('Freier Sprechstundentermin');
+            $event->description     = _('Dieser Sprechstundentermin ist noch nicht belegt.');
+        }
+
+        restoreLanguage();
+
+        $event->store();
     }
 }
