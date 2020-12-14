1a7ca4356b2eda961182162e2cf87ed8a43a8211
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2020-12-14 08:03:31 +0000

    fix history, re #10948
    git-svn-id: svn://develop.studip.de/studip/trunk@59611 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/migrations/Migrator.php
+++ lib/migrations/Migrator.php
@@ -290,9 +290,9 @@ class Migrator
         $migrations = $this->migrationClasses();
 
         // Determine correct target version
-        $this->target_version = $target_version
-                              ? (int) $target_version
-                              : $this->topVersion();
+        $this->target_version = $target_version === null
+                              ? $this->topVersion()
+                              : (int) $target_version;
 
         // Determine max version (might differ from max schema version in db in
         // development systems)
