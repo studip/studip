daf0ec4859c48a932de1134b1747977b78368a62
anoack <anoack@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2021-01-04 18:10:36 +0000

    re #11013
    git-svn-id: svn://develop.studip.de/studip/trunk@59747 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- db/studip_demo_data.sql
+++ db/studip_demo_data.sql
@@ -228,3 +228,16 @@ REPLACE INTO `user_inst` (`user_id`, `Institut_id`, `inst_perms`, `sprechzeiten`
 
 REPLACE INTO `user_studiengang` (`user_id`, `fach_id`, `semester`, `abschluss_id`) VALUES('e7a0a84b161f3e8c09b4a0a2e8a58147', '6b9ac09535885ca55e29dd011e377c0a', 2, '228234544820cdf75db55b42d1ea3ecc');
 REPLACE INTO `user_studiengang` (`user_id`, `fach_id`, `semester`, `abschluss_id`) VALUES('7e81ec247c151c02ffd479511e24cc03', 'f981c9b42ca72788a09da4a45794a737', 1, '228234544820cdf75db55b42d1ea3ecc');
+
+
+REPLACE INTO `news` (`news_id`, `topic`, `body`, `author`, `date`, `user_id`, `expire`, `allow_comments`, `chdate`, `chdate_uid`, `mkdate`) VALUES ('29f2932ce32be989022c6f43b866e744', 'Herzlich Willkommen!', '<!--HTML-->
+Das Stud.IP-Team heisst sie herzlich willkommen.<br />
+Bitte schauen Sie sich ruhig um!<br /><br />
+Wenn Sie das System selbst installiert haben und diese News sehen, haben Sie die Demonstrationsdaten in die Datenbank eingefügt. Wenn Sie produktiv mit dem System arbeiten wollen, sollten Sie diese Daten später wieder löschen, <strong>da die Passwörter der Accounts öffentlich bekannt sind</strong>.<br />
+ 
+<p>Wenn Sie mit der Stud.IP Open Source-Community in Kontakt treten wollen und Fragen oder Ideen haben, können Sie sich auf unserem eigenen Stud.IP-System mit uns austauschen: <a href="https://develop.studip.de/studip" class="link-extern" target="_blank" rel="noreferrer noopener">develop.studip.de</a></p>
+
+<p>Falls Sie die Einführung an einer Bildungseinrichtung planen und Unterstützung bei der Einführung oder Beratung zum Einsatz von digitalen Tools für die Online Lehre haben, steht Ihnen die Firma data-quest als Dienstleister rund um Stud.IP zur Seite: <a href="https://www.data-quest.de" class="link-extern" target="_blank" rel="noreferrer noopener">https://www.data-quest.de</a></p>', 'Root Studip', UNIX_TIMESTAMP(), '76ed43ef286fb55cf9e41beadb484a9f', 14562502, 1, UNIX_TIMESTAMP(), '', UNIX_TIMESTAMP());
+
+REPLACE INTO news_range VALUES ('29f2932ce32be989022c6f43b866e744', '76ed43ef286fb55cf9e41beadb484a9f');
+REPLACE INTO news_range VALUES ('29f2932ce32be989022c6f43b866e744', 'studip');
