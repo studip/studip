534be86b40790deebf61ae9c542c84a9ee396262
davka <davka@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2021-01-08 07:03:16 +0000

    revert changes, re #11046
    git-svn-id: svn://develop.studip.de/studip/trunk@59806 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/classes/auth_plugins/StudipAuthShib.class.php
+++ lib/classes/auth_plugins/StudipAuthShib.class.php
@@ -36,7 +36,6 @@ class StudipAuthShib extends StudipAuthSSO
             if (isset($this->local_domain)) {
                 $this->userdata['username'] =
                     str_replace('@'.$this->local_domain, '', $this->userdata['username']);
-                $this->local_domain = str_replace('mail.', '', $this->local_domain);
             }
         }
     }
