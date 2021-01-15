46ad56cb57a448d58ceec29ee59b03b7aefa6338
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2021-01-15 15:02:47 +0000

    add missing migration, re #11044
    git-svn-id: svn://develop.studip.de/studip/trunk@59884 c5b85792-5220-0410-ad23-ed5b6b8ad999

---
+++ db/migrations/20210108_tic11044_admin_course_notices.php
@@ -0,0 +1,49 @@
+<?php
+class Tic11044AdminCourseNotices extends Migration
+{
+    public function description()
+    {
+        return 'TIC #11044: Adds neccessary datafield that stores notice for courses';
+    }
+
+    public function up()
+    {
+        $query = "SELECT 1 + MAX(priority)
+                  FROM datafields
+                  WHERE object_type = 'sem'";
+        $priority = DBManager::get()->fetchColumn($query) ?: 0;
+
+        $query = "INSERT IGNORE INTO `datafields` (
+                    `datafield_id`, `name`,
+                    `object_type`, `object_class`,
+                    `edit_perms`, `view_perms`,
+                    `system`, `priority`,
+                    `mkdate`, `chdate`,
+                    `type`, `typeparam`,
+                    `is_required`, `is_userfilter`,
+                    `description`
+                  ) VALUES (
+                      :id, 'Notiz zu einer Veranstaltung',
+                      'sem', NULL,
+                      'admin', 'admin',
+                      1, :priority,
+                      UNIX_TIMESTAMP(), UNIX_TIMESTAMP(),
+                      'textarea', '',
+                      0, 0,
+                      'Enthält etwaige Notizen von Admins zu Veranstaltungen'
+                  )";
+        DBManager::get()->execute($query, [
+            ':id'       => md5(uniqid(__CLASS__, true)),
+            ':priority' => $priority,
+        ]);
+    }
+
+    public function down()
+    {
+        $query = "DELETE FROM datafields, datafields_entries
+                  FROM datafields
+                  JOIN datafields_entries USING (datafield_id)
+                  WHERE datafields.name = 'Notiz zu einer Veranstaltung'";
+        DBManager::get()->execute($query);
+    }
+}
