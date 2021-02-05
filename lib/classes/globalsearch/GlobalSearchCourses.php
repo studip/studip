e82d0d7967f3da45ca334c42e885369b3c52823b
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2021-02-05 12:18:19 +0000

    cast start_time to signed, fixes #10924
    git-svn-id: svn://develop.studip.de/studip/trunk@60140 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/classes/globalsearch/GlobalSearchCourses.php
+++ lib/classes/globalsearch/GlobalSearchCourses.php
@@ -101,7 +101,7 @@ class GlobalSearchCourses extends GlobalSearchModule implements GlobalSearchFull
                 {$seminar_type_condition}
                 {$semester_condition}
                 GROUP BY courses.Seminar_id
-                ORDER BY ABS(UNIX_TIMESTAMP() - start_time) ASC";
+                ORDER BY ABS(UNIX_TIMESTAMP() - CAST(start_time AS SIGNED)) ASC";
 
         if (Config::get()->IMPORTANT_SEMNUMBER) {
             $sql .= ", courses.`VeranstaltungsNummer`";
