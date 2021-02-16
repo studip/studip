e13bfb6f33c21823e097caef539b6831dbe5fbbc
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2021-02-16 11:48:07 +0000

    remove composer directory and compiled files from public/assets and ignore those from now on, re #11032
    git-svn-id: svn://develop.studip.de/studip/trunk@60321 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- composer/davefx/phplot/README.txt
+++
@@ -1,1 +0,0 @@
-phplot/README.txt
--- composer/mishal/iless/tests/ILess/Test/Parser/_fixtures/less.js/less/plugin/plugin-transitive.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use ILess\Node\AnonymousNode;
-
-$this->addFunctions([
-    'test-transitive' => function () {
-        return new AnonymousNode('transitive');
-    },
-]);
