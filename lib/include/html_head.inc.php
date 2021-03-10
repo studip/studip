4b55b279bd9de06e06b4e78518eb04f3d518b671
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2021-03-10 13:27:12 +0000

    set 'responsive-display' as early as possible on html node and hide sidebar before page is responsified, fixes #11203
    git-svn-id: svn://develop.studip.de/studip/trunk@60691 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/include/html_head.inc.php
+++ lib/include/html_head.inc.php
@@ -28,34 +28,39 @@ $getInstalledLanguages = function () {
     <script>
         CKEDITOR_BASEPATH = "<?= Assets::url('javascripts/ckeditor/') ?>";
         String.locale = "<?= htmlReady(strtr($_SESSION['_language'], '_', '-')) ?>";
-    </script>
-    <? if ($_SESSION['_language'] !== 'de_DE'): ?>
-        <link rel="localization" hreflang="<?= htmlReady(strtr($_SESSION['_language'], '_', '-')) ?>"
-              href="<?= URLHelper::getScriptLink('dispatch.php/localizations/' . $_SESSION['_language']) ?>" type="application/vnd.oftn.l10n+json">
-    <? endif ?>
 
-    <script>
-    document.querySelector('html').className = 'js';
-    window.STUDIP = {
-        ABSOLUTE_URI_STUDIP: "<?= $GLOBALS['ABSOLUTE_URI_STUDIP'] ?>",
-        ASSETS_URL: "<?= $GLOBALS['ASSETS_URL'] ?>",
-        CSRF_TOKEN: {
-            name: '<?=CSRFProtection::TOKEN?>',
-            value: '<? try {echo CSRFProtection::token();} catch (SessionRequiredException $e){}?>'
-        },
-        INSTALLED_LANGUAGES: <?= json_encode($getInstalledLanguages()) ?>,
-        STUDIP_SHORT_NAME: "<?= htmlReady(Config::get()->STUDIP_SHORT_NAME) ?>",
-        URLHelper: {
-            base_url: "<?= $GLOBALS['ABSOLUTE_URI_STUDIP'] ?>",
-            parameters: <?= json_encode(URLHelper::getLinkParams(), JSON_FORCE_OBJECT) ?>
-        },
-        jsupdate_enable: <?= json_encode(
-                         is_object($GLOBALS['perm']) &&
-                         $GLOBALS['perm']->have_perm('autor') &&
-                         PersonalNotifications::isActivated()) ?>,
-        wysiwyg_enabled: <?= json_encode((bool) Config::get()->WYSIWYG) ?>
-    }
+        document.querySelector('html').className = 'js';
+        setTimeout(() => {
+            // This needs to be put in a timeout since otherwise it will not match
+            if (window.matchMedia('(max-width: 767px)').matches) {
+                document.querySelector('html').classList.add('responsive-display');
+            }
+        }, 0);
+
+        window.STUDIP = {
+            ABSOLUTE_URI_STUDIP: "<?= $GLOBALS['ABSOLUTE_URI_STUDIP'] ?>",
+            ASSETS_URL: "<?= $GLOBALS['ASSETS_URL'] ?>",
+            CSRF_TOKEN: {
+                name: '<?=CSRFProtection::TOKEN?>',
+                value: '<? try {echo CSRFProtection::token();} catch (SessionRequiredException $e){}?>'
+            },
+            INSTALLED_LANGUAGES: <?= json_encode($getInstalledLanguages()) ?>,
+            STUDIP_SHORT_NAME: "<?= htmlReady(Config::get()->STUDIP_SHORT_NAME) ?>",
+            URLHelper: {
+                base_url: "<?= $GLOBALS['ABSOLUTE_URI_STUDIP'] ?>",
+                parameters: <?= json_encode(URLHelper::getLinkParams(), JSON_FORCE_OBJECT) ?>
+            },
+            jsupdate_enable: <?= json_encode(
+                             is_object($GLOBALS['perm']) &&
+                             $GLOBALS['perm']->have_perm('autor') &&
+                             PersonalNotifications::isActivated()) ?>,
+            wysiwyg_enabled: <?= json_encode((bool) Config::get()->WYSIWYG) ?>
+        }
     </script>
+<? if ($_SESSION['_language'] !== 'de_DE'): ?>
+    <link rel="localization" hreflang="<?= htmlReady(strtr($_SESSION['_language'], '_', '-')) ?>"
+          href="<?= URLHelper::getScriptLink('dispatch.php/localizations/' . $_SESSION['_language']) ?>" type="application/vnd.oftn.l10n+json">
+<? endif ?>
 
     <?= PageLayout::getHeadElements() ?>
 
