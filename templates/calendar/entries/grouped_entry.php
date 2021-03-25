db895eb3336e3e0dae00125a5fc930d5225331b1
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2021-03-25 07:32:56 +0000

    re #11320
    git-svn-id: svn://develop.studip.de/studip/trunk@60964 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- templates/calendar/entries/grouped_entry.php
+++ templates/calendar/entries/grouped_entry.php
@@ -25,10 +25,10 @@ $element_id = md5(uniqid());
         <?= $entry[0]['onClick'] ? 'onClick="STUDIP.Calendar.clickEngine(' . $entry[0]['onClick'] . ', this, event); return false;"' : '' ?>>
 
     <!-- for safari5 we need to set the height for the dl as well -->
-    <dl class="schedule-category<?= $entry[0]['color']?> <?= $calendar_view->getReadOnly() ? '' : 'hover' ?>" style="height: <?= $height ?>px;">        <dt>
+    <dl class="schedule-category<?= $entry[0]['color']?> <?= $calendar_view->getReadOnly() ? '' : 'hover' ?>" style="height: <?= $height ?>px;">
         <dt>
             <?= $entry[0]['start_formatted'] ?> - <?= $entry[0]['end_formatted'] ?>
-            <?= (count($heading) ? ', <b>' . htmlReady(implode(', ', $heading)) . '</b>' : '') ?>
+            <?= (count($heading) ? ', <strong>' . htmlReady(implode(', ', $heading)) . '</strong>' : '') ?>
         </dt>
         <dd>
             <? foreach ($entry as $element) :
