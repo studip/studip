5c74f8848686b457c712944472af43c3390fcc23
elmar <elmar@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2021-04-19 10:42:06 +0000

    link to course, not overview page, fixes #11424
    git-svn-id: svn://develop.studip.de/studip/trunk@61322 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/classes/globalsearch/GlobalSearchMyCourses.php
+++ lib/classes/globalsearch/GlobalSearchMyCourses.php
@@ -137,7 +137,7 @@ class GlobalSearchMyCourses extends GlobalSearchModule
             'id'            => $course->id,
             'number'        => self::mark($course->veranstaltungsnummer, $search),
             'name'          => self::mark($course->getFullname(), $search),
-            'url'           => URLHelper::getURL("dispatch.php/course/details/index/{$course->id}", [], true),
+            'url'           => URLHelper::getURL('seminar_main.php', ['cid' => $course->id], true),
             'date'          => $semester->token ?: $semester->name,
             'dates'         => $turnus_string,
             'has_children'  => count($course->children) > 0,
