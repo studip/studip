0a2c7dda4615db8ff56a494f4da85e3de46afaed
brueckner <brueckner@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2021-04-30 09:51:58 +0000

    check wiki page title in form, re #10489
    git-svn-id: svn://develop.studip.de/studip/trunk@61612 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/views/wiki/create.php
+++ app/views/wiki/create.php
@@ -1,11 +1,14 @@
 <p>
     <?= _('Hier können Sie eine neue Wiki-Seite erstellen.') ?>
+    <br />
+    <?= _('Bitte beachten Sie:') ?>
+    <?= _('Eckige Klammern und das Zeichen | sind im Titel nicht erlaubt.') ?>
 </p>
 
 <form action="<?= URLHelper::getLink('wiki.php', ['view' => 'editnew', 'lastpage' => $keyword]) ?>" method="post" class="default">
     <label>
         <span class="required"><?= _('Titel') ?></span>
-        <input required type="text" name="keyword"
+        <input required type="text" name="keyword" pattern="[^\][|]+"
                placeholder="<?= _('Name der Wiki-Seite') ?>">
     </label>
 
