799ac32bdf16060a9541bb02507b27bce8ab1ca6
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2021-05-18 07:14:04 +0000

    cleanup base components definition, re #10996, re #10736
    git-svn-id: svn://develop.studip.de/studip/trunk@61845 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- resources/vue/base-components.js
+++ resources/vue/base-components.js
@@ -1,18 +1,22 @@
+import Quicksearch from './components/Quicksearch.vue';
 import StudipActionMenu from './components/StudipActionMenu.vue';
 import StudipAssetImg from './components/StudipAssetImg.vue';
-import StudipProxyCheckbox from './components/StudipProxyCheckbox.vue';
-import StudipProxiedCheckbox from './components/StudipProxiedCheckbox.vue';
 import StudipDateTime from './components/StudipDateTime.vue';
+import StudipDialog from './components/StudipDialog.vue';
 import StudipFileSize from './components/StudipFileSize.vue';
 import StudipIcon from './components/StudipIcon.vue';
 // import StudipLoadingIndicator from './StudipLoadingIndicator.vue';
 import StudipMessageBox from './components/StudipMessageBox.vue';
+import StudipProxyCheckbox from './components/StudipProxyCheckbox.vue';
+import StudipProxiedCheckbox from './components/StudipProxiedCheckbox.vue';
 import StudipTooltipIcon from './components/StudipTooltipIcon.vue';
 
 const BaseComponents = {
+    Quicksearch,
     StudipActionMenu,
     StudipAssetImg,
     StudipDateTime,
+    StudipDialog,
     StudipFileSize,
     StudipIcon,
 //    StudipLoadingIndicator,
