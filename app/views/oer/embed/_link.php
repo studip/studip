364947c5017979e9171ab13c2214fc4c15808060
strohm <strohm@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2021-05-21 07:46:17 +0000

    views/oer/embed/_link: made text string more translatable, fixes #11532
    git-svn-id: svn://develop.studip.de/studip/trunk@61884 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/views/oer/embed/_link.php
+++ app/views/oer/embed/_link.php
@@ -1,6 +1,6 @@
 <div style="text-align: right;">
     <a href="<?= URLHelper::getLink("dispatch.php/oer/market/details/{$id}") ?>"
-       title="<?= htmlReady(sprintf(_('Zum %s'), Config::get()->OER_TITLE)) ?>">
+       title="<?= htmlReady(sprintf(_('Zum %s wechseln'), Config::get()->OER_TITLE)) ?>">
         <?= Icon::create('service')->asImg(['class' => 'text-bottom']) ?>
         <?= htmlReady($material['name']) ?>
     </a>
