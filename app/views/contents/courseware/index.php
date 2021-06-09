8b924442e709e703b3814dca6e8c950b912c1990
rlucke <rlucke@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2021-06-09 09:15:02 +0000

    ref #11575
    git-svn-id: svn://develop.studip.de/studip/trunk@62105 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/views/contents/courseware/index.php
+++ app/views/contents/courseware/index.php
@@ -1,23 +1,40 @@
 
 <div class="cw-content-projects">
-    <ul class="cw-tiles">
-        <? foreach($elements as $element) :?>
-            <li class="tile <?= htmlReady($element['payload']['color'])?>">
-                <a href="<?= URLHelper::getLink('dispatch.php/contents/courseware/courseware#/structural_element/'.$element['id']) ?>">
-                    <div class="preview-image" style="background-image: url(<?= htmlReady($element->getImageUrl()) ?>)" ></div>
-                    <div class="description">
-                        <header><?= htmlReady($element['title']) ?></header>
-                        <div class="description-text-wrapper">
-                            <p>
-                                <?= htmlReady($element['payload']['description']) ?>
-                            </p>
+    <? if(!empty($elements)): ?>
+        <ul class="cw-tiles">
+            <? foreach($elements as $element) :?>
+                <li class="tile <?= htmlReady($element['payload']['color'])?>">
+                    <a href="<?= URLHelper::getLink('dispatch.php/contents/courseware/courseware#/structural_element/'.$element['id']) ?>">
+                        <div class="preview-image" style="background-image: url(<?= htmlReady($element->getImageUrl()) ?>)" ></div>
+                        <div class="description">
+                            <header><?= htmlReady($element['title']) ?></header>
+                            <div class="description-text-wrapper">
+                                <p>
+                                    <?= htmlReady($element['payload']['description']) ?>
+                                </p>
+                            </div>
+                            <footer>
+                                <?= sprintf(ngettext('%d Seite', '%d Seiten', $element->countChildren()), $element->countChildren()); ?>
+                            </footer>
                         </div>
-                        <footer>
-                            <?= sprintf(ngettext('%d Seite', '%d Seiten', $element->countChildren()), $element->countChildren()); ?>
-                        </footer>
-                    </div>
+                    </a>
+                </li>
+            <? endforeach; ?>
+        </ul>
+    <? else: ?>
+        <div class="cw-contents-overview-teaser">
+            <div class="cw-contents-overview-teaser-content">
+                <header><?= _('Ihre persönlichen Lernmaterialien')?></header>
+                <p><?= _('Erstellen und Verwalten Sie hier ihre eigenen persönlichen Lernmaterialien in Form von ePorfolios, 
+                          Vorlagen für Veranstaltungen oder einfach nur persönliche Inhalte für das Studium. 
+                          Entwickeln Sie ihre eigenen (Lehr-)Materialien für Studium oder die Lehre und teilen diese mit anderen Nutzenden.')?></p>
+                <a class="button"
+                href="<?= $controller->link_for('contents/courseware/create_project', []) ?>"
+                data-dialog="size=700"
+                title="<?= _('Neues Lernmaterial anlegen') ?>">
+                    <?= _('Neues Lernmaterial anlegen') ?>
                 </a>
-            </li>
-        <? endforeach; ?>
-    </ul>
+            </div>
+        </div>
+    <? endif; ?> 
 </div>
