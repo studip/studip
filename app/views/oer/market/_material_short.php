63f908bd46a4f274e4a8866d19fe5e926ac23dfc
krassmus <krassmus@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2021-07-12 14:07:09 +0000

    fixes #11460
    git-svn-id: svn://develop.studip.de/studip/trunk@62505 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/views/oer/market/_material_short.php
+++ app/views/oer/market/_material_short.php
@@ -23,10 +23,9 @@
                 }
                 ?>
                 <?= Icon::create($icon, Icon::ROLE_CLICKABLE)->asImg(20, ['class' => "text-bottom"]) ?>
-                <?= htmlReady($material['name']) ?>
+                <div class="title"><?= htmlReady($material['name']) ?></div>
             </h1>
         </header>
         <div class="image" style="background-image: url(<?= $material->getLogoURL() ?>);<?= (!$material['front_image_content_type']) ? " background-size: 60% auto;" : "" ?>"></div>
     </a>
-<? $tags = $material->getTopics(); ?>
 </article>
