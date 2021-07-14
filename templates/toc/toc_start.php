ab0b70bbd28425f304835beebc225e2f48648d99
brueckner <brueckner@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2021-07-14 10:44:47 +0000

    re #11591
    git-svn-id: svn://develop.studip.de/studip/trunk@62521 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- templates/toc/toc_start.php
+++ templates/toc/toc_start.php
@@ -4,7 +4,7 @@
             <a class="navigate" href="<?= URLHelper::getLink('wiki.php',
                                         ['keyword' => htmlReady($wikiwikiweb)]) ?>  ">
                 <?= ($active)
-                    ? Icon::create('wiki', 'info_alt')->asImg(20, ['style' => 'padding-right: 5px'])
+                    ? Icon::create('wiki', 'info')->asImg(20, ['style' => 'padding-right: 5px'])
                     : Icon::create('wiki', 'clickable')->asImg(20, ['style' => 'padding-right: 5px'])
                 ?>
                 <?= ($wikiwikiweb == 'WikiWikiWeb')
