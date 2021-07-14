74d4d3836e8df2735eb4033728064189f7b7b5b1
hackl <hackl@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2021-07-14 08:21:15 +0000

    change appearance of contents tiles according to GUI talks, re #11666
    git-svn-id: svn://develop.studip.de/studip/trunk@62519 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- public/plugins_packages/core/ContentsWidget/templates/index.php
+++ public/plugins_packages/core/ContentsWidget/templates/index.php
@@ -7,17 +7,19 @@
             <? if ($navigation->isVisible() && $key !== 'overview'): ?>
                 <li class="content-item content-item-courseware">
                     <a href="<?= URLHelper::getLink($navigation->getURL()) ?>" class="content-item-link">
-                        <div class="content-item-title">
-                            <?= htmlReady($navigation->getTitle()) ?>
-                        </div>
                         <div class="content-item-img-wrapper">
                             <? if ($navigation->getImage()): ?>
                                 <?= $navigation->getImage()->asImg(32, $navigation->getLinkAttributes()) ?>
                             <? endif ?>
                         </div>
-                        <p class="content-item-description">
-                            <?= htmlReady($navigation->getDescription()) ?>
-                        </p>
+                        <div class="content-item-text">
+                            <p class="content-item-title">
+                                <?= htmlReady($navigation->getTitle()) ?>
+                            </p>
+                            <p class="content-item-description">
+                                <?= htmlReady($navigation->getDescription()) ?>
+                            </p>
+                        </div>
                     </a>
                 </li>
             <? endif ?>
